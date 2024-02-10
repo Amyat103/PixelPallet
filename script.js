96e09c71f21ec4eeea73df77fce73dec1d0ecdf7
Amyat103 <zhoudavid7312@gmail.com>
2024-02-09 23:44:31 -0500

    fix new grid stacking
    old grid is remove when new value is enter to make new grid

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 
 
 // TODO:
-// make 16x16 grid from js
-// find loop and make by appedning
+// everytime click remove grid
 // 
 // 
+// 
+// container div
+const containerDiv = document.querySelector(".container");
+
+
+// const innerContainer = document.querySelector(".innerDiv");
 
 const inputBox = document.querySelector("#gridSize");
 const inputButton = document.querySelector("#sizeButton");
 
-let gridSize = 0;
+let gridSize;
+makeGrid(10);
+
+
 
 const clickSize = inputButton.addEventListener("click", (e) => {
     gridSize = inputBox.value;
+    containerDiv.innerHTML = "";
     makeGrid(gridSize);
 })
 
 console.log(gridSize);
 
 
-const containerDiv = document.querySelector(".container");
-
 function makeGrid(gridSize) {
+    // temp div to remove easily
+    const innerContainer = document.createElement("div");
+    innerContainer.className = "innerDiv";
+
     for (let i = 0; i < gridSize; i++) {
 
         const row = document.createElement("div");
@@ -33,9 +44,12 @@ function makeGrid(gridSize) {
     
             row.appendChild(smallDiv);
         }
-    
-        containerDiv.appendChild(row);
+        
+        // row by row
+        innerContainer.appendChild(row);
     }
+    // append the whole thing
+    containerDiv.append(innerContainer);
 }
 
 
