e5f86b3ce7f660922fa22365c4a57f44aeade188
Amyat103 <zhoudavid7312@gmail.com>
2024-02-09 23:11:47 -0500

    add input, listener and new grid funciton
    wait for user to input size and create the grid size they want

--- script.js
+++ script.js
@@ -6,24 +6,38 @@
 // 
 // 
 
+const inputBox = document.querySelector("#gridSize");
+const inputButton = document.querySelector("#sizeButton");
+
+let gridSize = 0;
+
+const clickSize = inputButton.addEventListener("click", (e) => {
+    gridSize = inputBox.value;
+    makeGrid(gridSize);
+})
+
+console.log(gridSize);
+
 
 const containerDiv = document.querySelector(".container");
 
+function makeGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
 
-for (let i = 0; i < 16; i++) {
-
-    const row = document.createElement("div");
-    row.className = "row";
-
-    for (let j = 0; j < 16; j++) {
-        const smallDiv = document.createElement("div");
-        smallDiv.className = "blocks";
-
-        row.appendChild(smallDiv);
+        const row = document.createElement("div");
+        row.className = "row";
+    
+        for (let j = 0; j < gridSize; j++) {
+            const smallDiv = document.createElement("div");
+            smallDiv.className = "blocks";
+    
+            row.appendChild(smallDiv);
+        }
+    
+        containerDiv.appendChild(row);
     }
-
-    containerDiv.appendChild(row);
 }
+
 
 
 
